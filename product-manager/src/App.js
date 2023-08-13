1b8107ff4644f66c69e9efefbeb311f8e523a608
hanan757 <hanan.korabi@stud.hshl.de>
2023-08-14 00:38:13 +0200

    Basic Login component with validation and positioning (1h20mins)

--- product-manager/src/App.js
+++ product-manager/src/App.js
@@ -4,18 +4,21 @@ import ProductDetails from './Components/ProductDetails'
 import ProductForm from './Components/ProductForm'
 import ProductList from './Components/ProductList'
 import Search from './Components/Search'
+import { StyledEngineProvider as StyleProvider } from '@mui/material/styles';
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path='/' element={<Login />} />
-        <Route path='/products' element={<ProductList />} />
-        <Route path='/products/add' element={<ProductForm />} />
-        <Route path='/products/:id/edit' element={<ProductForm />} />
-        <Route path='/products/:id' element={<ProductDetails />} />
-        <Route path='/search' element={<Search />} />
-      </Routes>
-    </Router>
+    <StyleProvider injectFirst>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Login />} />
+          <Route path='/products' element={<ProductList />} />
+          <Route path='/products/add' element={<ProductForm />} />
+          <Route path='/products/:id/edit' element={<ProductForm />} />
+          <Route path='/products/:id' element={<ProductDetails />} />
+          <Route path='/search' element={<Search />} />
+        </Routes>
+      </Router>
+    </StyleProvider>
   );
 }
 
--- product-manager/src/Components/Login.js
+++ product-manager/src/Components/Login.js
@@ -1,10 +1,69 @@
-import React from "react";
+import React, { useState } from "react";
+import { Button, Typography, TextField, Box, Container } from '@mui/material'
 
 const Login = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [errorMessage, setError] = useState('')
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(username,password);
+        if (username === 'admin' && password === 'adminPW') {
+            localStorage.setItem('isLoggedIn', 'true');
+            window.location.href = '/products';
+        }
+        else {
+            setError('Wrong username or password!');
+        }
+    }
     return (
-        <div>
-            <h1>Login</h1>
-        </div>
+        <Container
+            component="main"
+            maxWidth="xs" >
+            <Box
+                component="form"
+                onSubmit={handleSubmit}
+                sx={{
+                    width: 350,
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: 'center',
+                    mt: 20,
+                    p: 5,
+                    border: "1px solid",
+                    borderRadius: 3
+                }}>
+                <Typography variant="h5" align="center">Sign in to NeuraCanvas</Typography>
+                <TextField
+                    sx={{ mt: 3, pt: 1 }}
+                    label="username"
+                    variant="standard"
+                    required
+                    fullWidth
+                    autoFocus
+                    error={errorMessage !== ''}
+                    onChange={(e) => setUsername(e.target.value)}
+                />
+                <TextField
+                    sx={{ mt: 3, pt: 1}}
+                    label="password"
+                    variant="standard"
+                    required
+                    fullWidth
+                    error={errorMessage !== ''}
+                    onChange={(e) => setPassword(e.target.value)} />
+                {errorMessage !== '' ?
+                 <Typography sx={{mt: 2}}variant=" subtitle1" color="error"> {errorMessage}</Typography>: <></>}
+                <Button
+                    sx={{ mt: 3, py: 1.5, px: 2}}
+                    align="center"
+                    type="submit"
+                    variant="standard"
+                    color="blue">
+                    Sign in
+                </Button>
+            </Box>
+        </Container>
     );
 }
 
